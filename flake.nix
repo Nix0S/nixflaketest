b8ee18309c810f724c44f2fa67bd54287310d4e9
Nix0S <github.j344l@simplelogin.com>
2023-02-08 11:50:32 -0600

    revised the snowfall-lib.url to include /dev

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
     # Snowfall Lib
-    snowfall-lib.url = "github:snowfallorg/lib";
+    snowfall-lib.url = "github:snowfallorg/lib/dev";
     snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
     # Snowfall Flake
